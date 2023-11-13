9edd681b32f160d8b1e6321511cf72dcf09ea311
raditya2002 <sayaiok2002@gmail.com>
2023-11-13 15:18:19 +0700

    first commit

---
+++ GUI_TugasMod8/Form1.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CtoR = New System.Windows.Forms.Button()
+        Me.CtoF = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.CtoK = New System.Windows.Forms.Button()
+        Me.RtoC = New System.Windows.Forms.Button()
+        Me.RtoF = New System.Windows.Forms.Button()
+        Me.RtoK = New System.Windows.Forms.Button()
+        Me.FtoC = New System.Windows.Forms.Button()
+        Me.FtoR = New System.Windows.Forms.Button()
+        Me.FtoK = New System.Windows.Forms.Button()
+        Me.KtoR = New System.Windows.Forms.Button()
+        Me.KtoC = New System.Windows.Forms.Button()
+        Me.KtoF = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CtoR
+        '
+        Me.CtoR.Location = New System.Drawing.Point(15, 85)
+        Me.CtoR.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.CtoR.Name = "CtoR"
+        Me.CtoR.Size = New System.Drawing.Size(56, 19)
+        Me.CtoR.TabIndex = 0
+        Me.CtoR.Text = "C to R"
+        Me.CtoR.UseVisualStyleBackColor = True
+        '
+        'CtoF
+        '
+        Me.CtoF.Location = New System.Drawing.Point(75, 108)
+        Me.CtoF.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.CtoF.Name = "CtoF"
+        Me.CtoF.Size = New System.Drawing.Size(56, 19)
+        Me.CtoF.TabIndex = 1
+        Me.CtoF.Text = "C to F"
+        Me.CtoF.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(15, 47)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(140, 23)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(313, 24)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Tugas Mod 8 RPLBK Konversi Suhu"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(11, 197)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(174, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Anggota Kelompok 23 :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 230)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Joane Eva D"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(12, 247)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(130, 17)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Ahmad Raditya AW"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(12, 264)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(99, 17)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "M Rofiul Anam"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(13, 281)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(98, 17)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Dimsa Mutiara"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(394, 20)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 20)
+        Me.Label4.TabIndex = 11
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(439, 53)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 17)
+        Me.Label8.TabIndex = 12
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(524, 53)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 17)
+        Me.Label9.TabIndex = 13
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(64, 158)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 13)
+        Me.Label10.TabIndex = 14
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(287, 53)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(0, 17)
+        Me.Label11.TabIndex = 15
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(370, 53)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(0, 17)
+        Me.Label12.TabIndex = 16
+        '
+        'CtoK
+        '
+        Me.CtoK.Location = New System.Drawing.Point(135, 131)
+        Me.CtoK.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.CtoK.Name = "CtoK"
+        Me.CtoK.Size = New System.Drawing.Size(56, 19)
+        Me.CtoK.TabIndex = 17
+        Me.CtoK.Text = "C to K"
+        Me.CtoK.UseVisualStyleBackColor = True
+        '
+        'RtoC
+        '
+        Me.RtoC.Location = New System.Drawing.Point(75, 85)
+        Me.RtoC.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RtoC.Name = "RtoC"
+        Me.RtoC.Size = New System.Drawing.Size(56, 19)
+        Me.RtoC.TabIndex = 18
+        Me.RtoC.Text = "R to C"
+        Me.RtoC.UseVisualStyleBackColor = True
+        '
+        'RtoF
+        '
+        Me.RtoF.Location = New System.Drawing.Point(135, 108)
+        Me.RtoF.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RtoF.Name = "RtoF"
+        Me.RtoF.Size = New System.Drawing.Size(56, 19)
+        Me.RtoF.TabIndex = 19
+        Me.RtoF.Text = "R to F"
+        Me.RtoF.UseVisualStyleBackColor = True
+        '
+        'RtoK
+        '
+        Me.RtoK.Location = New System.Drawing.Point(15, 154)
+        Me.RtoK.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RtoK.Name = "RtoK"
+        Me.RtoK.Size = New System.Drawing.Size(56, 19)
+        Me.RtoK.TabIndex = 20
+        Me.RtoK.Text = "R to K"
+        Me.RtoK.UseVisualStyleBackColor = True
+        '
+        'FtoC
+        '
+        Me.FtoC.Location = New System.Drawing.Point(75, 154)
+        Me.FtoC.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.FtoC.Name = "FtoC"
+        Me.FtoC.Size = New System.Drawing.Size(56, 19)
+        Me.FtoC.TabIndex = 21
+        Me.FtoC.Text = "F to C"
+        Me.FtoC.UseVisualStyleBackColor = True
+        '
+        'FtoR
+        '
+        Me.FtoR.Location = New System.Drawing.Point(15, 131)
+        Me.FtoR.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.FtoR.Name = "FtoR"
+        Me.FtoR.Size = New System.Drawing.Size(56, 19)
+        Me.FtoR.TabIndex = 22
+        Me.FtoR.Text = "F to R"
+        Me.FtoR.UseVisualStyleBackColor = True
+        '
+        'FtoK
+        '
+        Me.FtoK.Location = New System.Drawing.Point(135, 85)
+        Me.FtoK.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.FtoK.Name = "FtoK"
+        Me.FtoK.Size = New System.Drawing.Size(56, 19)
+        Me.FtoK.TabIndex = 23
+        Me.FtoK.Text = "F to K"
+        Me.FtoK.UseVisualStyleBackColor = True
+        '
+        'KtoR
+        '
+        Me.KtoR.Location = New System.Drawing.Point(15, 108)
+        Me.KtoR.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.KtoR.Name = "KtoR"
+        Me.KtoR.Size = New System.Drawing.Size(56, 19)
+        Me.KtoR.TabIndex = 24
+        Me.KtoR.Text = "K to R"
+        Me.KtoR.UseVisualStyleBackColor = True
+        '
+        'KtoC
+        '
+        Me.KtoC.Location = New System.Drawing.Point(135, 154)
+        Me.KtoC.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.KtoC.Name = "KtoC"
+        Me.KtoC.Size = New System.Drawing.Size(56, 19)
+        Me.KtoC.TabIndex = 25
+        Me.KtoC.Text = "K to C"
+        Me.KtoC.UseVisualStyleBackColor = True
+        '
+        'KtoF
+        '
+        Me.KtoF.Location = New System.Drawing.Point(75, 131)
+        Me.KtoF.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.KtoF.Name = "KtoF"
+        Me.KtoF.Size = New System.Drawing.Size(56, 19)
+        Me.KtoF.TabIndex = 26
+        Me.KtoF.Text = "K to F"
+        Me.KtoF.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 366)
+        Me.Controls.Add(Me.KtoF)
+        Me.Controls.Add(Me.KtoC)
+        Me.Controls.Add(Me.KtoR)
+        Me.Controls.Add(Me.FtoK)
+        Me.Controls.Add(Me.FtoR)
+        Me.Controls.Add(Me.FtoC)
+        Me.Controls.Add(Me.RtoK)
+        Me.Controls.Add(Me.RtoF)
+        Me.Controls.Add(Me.RtoC)
+        Me.Controls.Add(Me.CtoK)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CtoF)
+        Me.Controls.Add(Me.CtoR)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "S"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CtoR As Button
+    Friend WithEvents CtoF As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents CtoK As Button
+    Friend WithEvents RtoC As Button
+    Friend WithEvents RtoF As Button
+    Friend WithEvents RtoK As Button
+    Friend WithEvents FtoC As Button
+    Friend WithEvents FtoR As Button
+    Friend WithEvents FtoK As Button
+    Friend WithEvents KtoR As Button
+    Friend WithEvents KtoC As Button
+    Friend WithEvents KtoF As Button
+End Class
